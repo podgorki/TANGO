4915fd39c984e1c2bdc8c62f577aa2b749d3d042
Stefan Podgorski <stefan.podgorski@adelaide.edu.au>
2025-02-25 14:07:17 +1030

    refactored robohop.py for consistent variable naming conventions

--- libs/control/robohop.py
+++ libs/control/robohop.py
@@ -2,32 +2,35 @@ import numpy as np
 
 from libs.commons import utils_viz
 
-def control_with_mask(plsCoords_or_semantic, goalMask, v=0.05, gain=0.2, tao=10):
-    H, W = goalMask.shape
 
-    if isinstance(plsCoords_or_semantic, np.ndarray):
-        semantic = plsCoords_or_semantic
+def control_with_mask(
+        pls_coords_or_semantic, goal_mask,
+        v: float = 0.05, gain: float = 0.2, tao: int = 10):
+    height, width = goal_mask.shape
+
+    if isinstance(pls_coords_or_semantic, np.ndarray):
+        semantic = pls_coords_or_semantic
         instances = np.unique(semantic)
 
         coords, pls = [], []
         for i in range(len(instances)):
             points = np.argwhere(semantic == instances[i])
             coords.append(np.array(points.mean(0)))
-            pls.append(goalMask[points[0, 0], points[0, 1]])
+            pls.append(goal_mask[points[0, 0], points[0, 1]])
         # NOTE: Coords are in (x=c,y=r) format
         coords = np.fliplr(np.array(coords))
         pls = np.array(pls)
     else:
-        pls, coords = plsCoords_or_semantic
+        pls, coords = pls_coords_or_semantic
 
-    plsMask = pls < 99
-    if plsMask.sum() <= 1:
-        return 0.0, 0.1, np.zeros((H,W,3))
-    pls = pls[plsMask]
-    coords = coords[plsMask]
-    outliers = goalMask >= 99
-    goalMask[outliers] = pls.max()
-    goalMask = pls.max() - goalMask  # to match the visImg color scheme
+    pls_mask = pls < 99
+    if pls_mask.sum() <= 1:
+        return 0.0, 0.1, np.zeros((height, width, 3))
+    pls = pls[pls_mask]
+    coords = coords[pls_mask]
+    outliers = goal_mask >= 99
+    goal_mask[outliers] = pls.max()
+    goal_mask = pls.max() - goal_mask  # to match the visualize_image color scheme
 
     weights = np.ones_like(pls)
     if np.unique(pls).shape[0] == 1:
@@ -37,15 +40,17 @@ def control_with_mask(plsCoords_or_semantic, goalMask, v=0.05, gain=0.2, tao=10)
         weights = np.exp(tao * weights)
         weights = weights / weights.sum()
 
-    coordsRef = coords.copy()
-    coordsRef[:, 0] = W // 2
-    weightedSum = (weights[:, None] * (coords - coordsRef)).sum(0)
-    x_off = weightedSum[0]
-    w = -x_off * gain / (W // 2)
+    coords_ref = coords.copy()
+    coords_ref[:, 0] = width // 2
+    weighted_sum = (weights[:, None] * (coords - coords_ref)).sum(0)
+    x_off = weighted_sum[0]
+    w = -x_off * gain / (width // 2)
     v = v
 
     colors, norm = utils_viz.value2color(weights, cmName='viridis')
-    visImg = utils_viz.visualize_flow(coords, coordsRef, goalMask, colors, norm, weights, fwdVals=None, display=False,
-                            colorbar=False).astype(float) / 255.0
-    visImg[outliers] = [255, 255, 255]
-    return v, w, visImg
+    visualize_image = utils_viz.visualize_flow(
+        coords, coords_ref, goal_mask, colors, norm, weights, fwdVals=None, display=False,
+        colorbar=False
+    ).astype(float) / 255.0
+    visualize_image[outliers] = [255, 255, 255]
+    return v, w, visualize_image
